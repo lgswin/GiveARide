7499f0798ef1a6f5225534b21dcf133c282b1ac8
GsLee <lgswin@gmail.com>
2025-03-07 13:19:03 -0500

    update

--- Navigations/TabNavigation.tsx
+++ Navigations/TabNavigation.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import FontAwesome from '@expo/vector-icons/FontAwesome';
 import AntDesign from '@expo/vector-icons/AntDesign';
+import { Text } from "react-native";
 
 import HomeScreen from '../Screens/HomeScreen';
 import ListScreen from '../Screens/ListScreen';
@@ -16,27 +17,21 @@ const TabNavigation = () => {
         name="Home" 
         component={HomeScreen} 
         options={{
-            tabBarIcon: ({ color }) => (
-                <AntDesign name="home" size={24} color={color} />
-              )
+            tabBarIcon: ({ color }) => <Text style={{ fontSize: 24 }}>🏠</Text>,
         }}
       />
       <Tab.Screen 
         name="List" 
         component={ListScreen} 
         options={{
-            tabBarIcon: ({ color }) => (
-                <FontAwesome name="list" size={24} color={color} />
-              )
+            tabBarIcon: ({ color }) => <Text style={{ fontSize: 24 }}>📋</Text>,
         }}
       />
       <Tab.Screen 
         name="Profile" 
         component={ProfileScreen} 
         options={{
-            tabBarIcon: ({ color }) => (
-                <AntDesign name="profile" size={24} color={color} />
-              )
+            tabBarIcon: ({ color }) => <Text style={{ fontSize: 24 }}>👤</Text>,
         }}
       />
     </Tab.Navigator>
