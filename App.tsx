de73131208821663054534cd6cb06efdc055b668
GsLee <lgswin@gmail.com>
2025-03-06 23:06:36 -0500

    add tab navigation

--- App.tsx
+++ App.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import TabNavigation from './Navigations/TabNavigation';
 import LoginScreen from "./screens/LoginScreen";
 import SignUpScreen from "./screens/SignUpScreen";
 import HomeScreen from "./screens/HomeScreen"; // 홈 화면 추가
@@ -10,11 +11,7 @@ const Stack = createNativeStackNavigator();
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="Home" component={HomeScreen} />
-        <Stack.Screen name="Login" component={LoginScreen} />
-        <Stack.Screen name="SignUp" component={SignUpScreen} />
-      </Stack.Navigator>
+        <TabNavigation />
     </NavigationContainer>
   );
 }
---
+++ Navigations/TabNavigation.tsx
@@ -0,0 +1,46 @@
+import React from 'react';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import FontAwesome from '@expo/vector-icons/FontAwesome';
+import AntDesign from '@expo/vector-icons/AntDesign';
+
+import HomeScreen from '../screens/HomeScreen';
+import LoginScreen from '../screens/LoginScreen';
+import SignupScreen from '../screens/SignUpScreen';
+
+const Tab = createBottomTabNavigator();
+
+const TabNavigation = () => {
+  return (
+    <Tab.Navigator>
+      <Tab.Screen
+        name="Home" 
+        component={HomeScreen} 
+        options={{
+            tabBarIcon: ({ color }) => (
+                <AntDesign name="home" size={24} color={color} />
+              )
+        }}
+      />
+      <Tab.Screen 
+        name="Login" 
+        component={LoginScreen} 
+        options={{
+            tabBarIcon: ({ color }) => (
+                <FontAwesome name="list" size={24} color={color} />
+              )
+        }}
+      />
+      <Tab.Screen 
+        name="Signup" 
+        component={SignupScreen} 
+        options={{
+            tabBarIcon: ({ color }) => (
+                <AntDesign name="profile" size={24} color={color} />
+              )
+        }}
+      />
+    </Tab.Navigator>
+  );
+};
+
+export default TabNavigation;
