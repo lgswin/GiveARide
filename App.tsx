9a55ee7c1e27296794d7c2fb2a46cd53da128489
GsLee <lgswin@gmail.com>
2025-03-07 10:09:29 -0500

    clean

--- App.tsx
+++ App.tsx
@@ -32,7 +32,7 @@ const App: React.FC = () => {
 
   return (
     <NavigationContainer>
-      <Stack.Navigator>
+      <Stack.Navigator screenOptions={{ headerShown: false }}>
         {!user ? (
           <Stack.Screen name="Login" component={LoginScreen} />
         ) : (
